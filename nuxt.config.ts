7c2e40c2e4262bc617c7bf52df93be039f3be1ea
Lpuena <lpuena@outlook.com>
2025-01-04 21:35:41 +0800

    chore: change nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,12 @@ export default defineNuxtConfig({
   app: {
     baseURL: '/tool/',
   },
+  nitro: {
+    prerender: {
+      crawlLinks: false, // 禁用路由抓取
+      routes: [], // 明确指定需要预渲染的路由
+    },
+  },
 
   devtools: { enabled: true },
   compatibilityDate: '2025-01-03',
